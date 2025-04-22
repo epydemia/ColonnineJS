44ac26da2065fb31ab08d2d4c5d2e2a1dfb2ffce
Daniele Franco <epydemia@gmail.com>
2025-04-22 10:23:00 +0200

    Fixed size of the Marker

--- js/mappaUI.js
+++ js/mappaUI.js
@@ -7,9 +7,17 @@ export function aggiornaUserMarker(lat, lon, heading) {
     if (!userMarker) {
       const icon = L.divIcon({
         className: 'user-heading-icon',
-        html: '<div class="marker-blu"><span class="freccia">▲</span></div>',
-        iconSize: [24, 24],
-        iconAnchor: [12, 12]
+        html: `
+          <div class="marker-blu">
+            <span class="freccia">
+              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <polygon points="12,0 22,24 2,24" />
+              </svg>
+            </span>
+          </div>
+        `,
+        iconSize: [64, 64],
+        iconAnchor: [32, 32]
       });
       userMarker = L.marker([lat, lon], { icon }).addTo(window.leafletMap);
     } else {
